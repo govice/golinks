0644741bc7654618874273563f4aaa8d8153d209
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-06-20 19:11:36 -0400

    demonstrate blockchain new and add methods

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
-import(
-	"time"
-	"fmt"
-	"github.com/LaughingCabbage/goLinks/types/block"
+import (
+	"github.com/LaughingCabbage/goLinks/types/blockchain"
 
 )
 
+
 func main() {
-	t0 := time.Now()
-	fmt.Println(t0.String())
-
-	rootBlock := block.New()
-
-	fmt.Println(rootBlock.Parenthash)
+	blkchain := blockchain.New()
+	blkchain.Add("NewSTring")
+	blkchain.Add("NewSTring")
+	blkchain.Print()
 
 }
