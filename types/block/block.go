62699b5988011acde66607db232bb637754f4da5
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-06-20 14:24:41 -0400

    create gob encoder to to return byte block for hashing

--- types/block/block.go
+++ types/block/block.go
@@ -1,26 +1,42 @@
 package block
 
-import(
-	//"fmt"
+import (
+	"fmt"
 	"time"
-	//"hash"
 	"crypto/sha512"
+	"bytes"
+	"encoding/gob"
+	"log"
 )
+
 type Block struct {
-	Index 		int
-	Timestamp 	time.Time
-	Data 		string
-	Parenthash 	[]uint8
-	Blockhash 	[]uint8
-
+	Index      int
+	Timestamp  time.Time
+	Data       string
+	Parenthash []uint8
+	Blockhash  []uint8
 }
 
-func New() Block{
-	block := Block{-1, time.Now(), "TEST DATAA", nil, nil}
+func New() Block {
+	blk := Block{-1, time.Now(), "TEST DATAA", nil, nil}
 	hash := sha512.New()
-	hash.Write([]byte(block.Data))
-	block.Parenthash = hash.Sum(nil)
-	return block
+	hash.Write([]byte(blk.Data))
+	blk.Parenthash = hash.Sum(nil)
+	hash = sha512.New()
+	hash.Write(EncodeGob(blk))
+	blk.Blockhash = hash.Sum(nil)
+	fmt.Println(blk)
+	return blk
 }
 
-//func Computehash()
+func EncodeGob(block Block) []byte{
+	//we need to serialize everything but the current block hash
+	//create a buffer, encode gob to buffer, return serial byte array
+	buffer := bytes.Buffer{}
+	blockgob := gob.NewEncoder(&buffer)
+	err := blockgob.Encode(Block{block.Index, block.Timestamp, block.Data, block.Parenthash, nil})
+	if err != nil {
+		log.Fatal("failed to encode block into Gob")
+	}
+	return buffer.Bytes()
+}
