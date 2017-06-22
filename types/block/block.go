8d07e6ad79c08ad3fdf17a864290636de4516d65
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-06-21 23:20:05 -0400

    create hash method. create validate method. begin adding error handling.

--- types/block/block.go
+++ types/block/block.go
@@ -6,14 +6,15 @@ import (
 	"bytes"
 	"encoding/gob"
 	"log"
+	"errors"
 )
 
 type Block struct {
 	Index      int
 	Timestamp  time.Time
 	Data       string
-	Parenthash []uint8
-	Blockhash  []uint8
+	Parenthash []byte
+	Blockhash  []byte
 }
 
 
@@ -28,9 +29,7 @@ func New(index int, data string, parent []uint8) Block {
 	}else{
 		blk.Parenthash = parent
 	}
-	blkhash := sha512.New()
-	blkhash.Write(EncodeGob(blk))
-	blk.Blockhash = blkhash.Sum(nil)
+	blk.Blockhash = Hash(blk)
 	return blk
 }
 
@@ -44,4 +43,26 @@ func EncodeGob(block Block) []byte{
 		log.Fatal("failed to encode block into Gob")
 	}
 	return buffer.Bytes()
+}
+
+//calculate and return the hash for a block
+func Hash(block Block) []uint8{
+	blkhash := sha512.New()
+	blkhash.Write(EncodeGob(block))
+	return blkhash.Sum(nil)
+}
+
+//need to check index, and hashes
+func Validate(prev, current Block) error{
+	if prev.Index+1 != current.Index {
+		return errors.New("block indexes do not corrilate")
+	}
+	if !bytes.Equal(prev.Blockhash, current.Parenthash){
+		return errors.New("block parent child hashes do not corrilate")
+	}
+	if !bytes.Equal(Hash(current), current.Blockhash) {
+		return errors.New("current block's hash is not valid")
+	}
+	return nil
+	
 }
