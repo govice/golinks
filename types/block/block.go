d263fbb037b86105c4a7b6763f58605d2e7261f3
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-06-25 11:50:23 -0400

    Add binary marshal/unmarshal and incorporate into hashing. Note: rebuilding of chain will currently require rehashing of individual blocks. O(N)!

--- types/block/block.go
+++ types/block/block.go
@@ -4,9 +4,9 @@ import (
 	"time"
 	"crypto/sha512"
 	"bytes"
-	"encoding/gob"
-	"log"
 	"errors"
+	"fmt"
+	"log"
 )
 
 type Block struct {
@@ -33,36 +33,42 @@ func New(index int, data string, parent []uint8) Block {
 	return blk
 }
 
-func EncodeGob(block Block) []byte{
-	//we need to serialize everything but the current block hash
-	//create a buffer, encode gob to buffer, return serial byte array
-	buffer := bytes.Buffer{}
-	blockgob := gob.NewEncoder(&buffer)
-	err := blockgob.Encode(Block{block.Index, block.Timestamp, block.Data, block.Parenthash, nil})
-	if err != nil {
-		log.Fatal("failed to encode block into Gob")
-	}
-	return buffer.Bytes()
-}
 
 //calculate and return the hash for a block
 func Hash(block Block) []uint8{
 	blkhash := sha512.New()
-	blkhash.Write(EncodeGob(block))
+	buff, err := block.MarshalBinary()
+	if err != nil {
+		log.Fatal("Failed to hash block")
+	}
+	blkhash.Write(buff)
 	return blkhash.Sum(nil)
 }
 
 //need to check index, and hashes
 func Validate(prev, current Block) error{
 	if prev.Index+1 != current.Index {
-		return errors.New("block indexes do not corrilate")
+		return errors.New("block indexes do not correlate")
 	}
 	if !bytes.Equal(prev.Blockhash, current.Parenthash){
-		return errors.New("block parent child hashes do not corrilate")
+		return errors.New("block parent child hashes do not correlate")
 	}
 	if !bytes.Equal(Hash(current), current.Blockhash) {
+		fmt.Println("Hash: ", Hash(current))
+		fmt.Println("current: ", current.Blockhash)
 		return errors.New("current block's hash is not valid")
 	}
 	return nil
-	
+}
+
+func (block Block) MarshalBinary() ([]byte, error){
+	var buffer bytes.Buffer
+	fmt.Fprintln(&buffer, block.Index, block.Timestamp, block.Data, block.Parenthash)
+	return buffer.Bytes(), nil
+}
+
+func (block *Block) UnmarshalBinary(data []byte) error {
+	buffer := bytes.NewBuffer(data)
+	_, err := fmt.Fscanln(buffer, &block.Index, &block.Timestamp, &block.Data, &block.Parenthash)
+	return err
 }
