3a819829bc7cb44a167a69eaa359f042e8442196
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-06-28 02:07:25 -0400

    begin developing client API

--- network/client/client.go
+++ network/client/client.go
@@ -1,1 +1,39 @@
 package client
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"log"
+)
+
+type Client struct {
+	serverAddress string
+	serverPort    string
+	privateKey    *rsa.PrivateKey
+	publicKey     *rsa.PublicKey
+	serverKey     *rsa.PublicKey
+}
+
+//New creates a new client and provides the server address.
+func (client *Client) New(serverAddr, serverPort string) {
+	client.serverAddress = serverAddr
+	client.serverPort = serverPort
+	var err error
+	client.privateKey, err = rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatal("failed to generate RSA private key ", err)
+	}
+
+	//entropy := []byte("laughingcabbage%$#@")
+	//label := []byte("message")
+
+	/*
+		rng := rand.Reader
+
+		ciphertext, err := rsa.EncryptOAEP(sha512.New(), rng, &client.publicKey, entropy, label)
+		if err != nil {
+			//		log.Fatal("Error in encryption ", err)
+		}
+		fmt.Println("ciphertext ", ciphertext)
+	*/
+}
