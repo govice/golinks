604bd66830af40ffd3ad79153181ab6d431058b7
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-06-27 21:54:20 -0400

    add new client and server msg repeater

--- app/client/client_app.go
+++ app/client/client_app.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("running client...")
+	fmt.Println("executing client...")
+
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal("failed to connect ", err)
+	}
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("text to send: ")
+		data, _ := reader.ReadString('\n')
+
+		fmt.Fprintf(conn, data+"\n")
+
+		msg, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Server reply: " + msg)
+	}
 }
--- app/server/server_app.go
+++ app/server/server_app.go
@@ -3,10 +3,30 @@ package main
 import (
 	"fmt"
 
-	"github.com/LaughingCabbage/goLinks/network/server"
+	"bufio"
+	"log"
+	"net"
+	"strings"
 )
 
 func main() {
-	fmt.Println("executing server...")
-	server.Listen()
+	fmt.Println("running server...")
+
+	client, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("listner could not connect", err)
+	}
+
+	conn, err := client.Accept()
+
+	for {
+		// will listen for message to process ending in newline (\n)
+		msg, _ := bufio.NewReader(conn).ReadString('\n')
+		// output message received
+		fmt.Print("Message Received:", string(msg))
+		// sample process for string received
+		newmessage := strings.ToUpper(msg)
+		// send new string back to client
+		conn.Write([]byte(newmessage + "\n"))
+	}
 }
