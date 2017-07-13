7091c8ff2ceb07aa3fbf1b8025b4976e8fa21200
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-07-13 00:33:59 -0400

    begin constructing command line client

--- app/client/client_app.go
+++ app/client/client_app.go
@@ -1,9 +1,43 @@
 package main
 
 import (
-	"github.com/LaughingCabbage/goLinks/network/client"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
 )
 
+//var gopherType string
+
+const (
+	defaultGopher = "pocket"
+	usage         = "the variety of gopher"
+)
+
+func main() {
+	//gopherType := flag.String("command", "cmd", "This is the help menu")
+
+	goph := flag.String("word", "bar", "a string")
+
+	//flag.StringVar(gopherType, "gopher_type", defaultGopher, usage)
+	//flag.StringVar(gopherType, "g", defaultGopher, usage+" (shorthand)")
+	flag.Parse()
+	fmt.Println(flag.Args())
+	fmt.Println(*goph)
+	//fmt.Println(stringFlag)
+	app := cli.NewApp()
+	app.Name = "greet"
+	app.Usage = "fight the loneliness!"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Hello friend!")
+		return nil
+	}
+
+	app.Run(os.Args)
+}
+
+/*
 func main() {
 	peer := client.New("localhost", "8080")
 	err := peer.Connect()
@@ -16,3 +50,4 @@ func main() {
 		peer.Listen()
 	}
 }
+*/
