9522abbb502ef269895c0c12ad969559727e0850
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-11-09 18:16:51 -0500

    move test to network

--- network/network_test.go
+++ network/network_test.go
@@ -1,10 +1,53 @@
 package network
 
-import "testing"
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+	"testing"
+
+	"github.com/LaughingCabbage/goLinks/types/block"
+)
 
 func TestGenerateKeyPair(t *testing.T) {
 	_, err := GenerateKeyPair()
 	if err != nil {
 		t.Error("faield to generate key pair", err)
+	}
+}
+
+func TestBinaryConverters(t *testing.T) {
+	var network bytes.Buffer
+
+	enc := gob.NewEncoder(&network)
+	dec := gob.NewDecoder(&network)
+
+	blk := block.New(0, []byte("data"), nil)
+	err := enc.Encode(blk)
+	if err != nil {
+		t.Error("Binary Encoding Failed", err)
+	}
+
+	var b block.Block
+	err = dec.Decode(&b)
+	if err != nil {
+		t.Error("Binary Decoding Failed ", err)
+	}
+	if blk.Index != b.Index {
+		t.Error("Block indexes do not corrolate")
+	}
+	if blk.Timestamp != b.Timestamp {
+		log.Print(blk.Timestamp)
+		log.Print(b.Timestamp)
+		t.Error("block timestamps do not corrolate")
+	}
+	if bytes.Equal(blk.Data, b.Data) {
+		t.Error("Block data does not corrolate")
+	}
+	if !bytes.Equal(blk.Parenthash, b.Parenthash) {
+		t.Error("BLock parent hashes do not corrolate")
+	}
+	if !bytes.Equal(blk.Blockhash, b.Blockhash) {
+		t.Error("Block hashes do not corrolate")
 	}
 }
--- types/block/block_test.go
+++ types/block/block_test.go
@@ -1,59 +1,26 @@
 package block
 
 import (
-	"bytes"
-	"encoding/gob"
+	"log"
 	"testing"
 )
-
-func TestBinaryConverters(t *testing.T) {
-	var network bytes.Buffer
-
-	enc := gob.NewEncoder(&network)
-	dec := gob.NewDecoder(&network)
-
-	blk := New(0, "data", nil)
-	err := enc.Encode(blk)
-	if err != nil {
-		t.Error("Binary Encoding Failed", err)
-	}
-
-	var b Block
-	err = dec.Decode(&b)
-	if err != nil {
-		t.Error("Binary Decoding Failed ", err)
-	}
-	if blk.Index != b.Index {
-		t.Error("Block indexes do not corrolate")
-	}
-	if blk.Timestamp != b.Timestamp {
-		t.Error("block timestamps do not corrolate")
-	}
-	if blk.Data != b.Data {
-		t.Error("Block data does not corrolate")
-	}
-	if !bytes.Equal(blk.Parenthash, b.Parenthash) {
-		t.Error("BLock parent hashes do not corrolate")
-	}
-	if !bytes.Equal(blk.Blockhash, b.Blockhash) {
-		t.Error("Block hashes do not corrolate")
-	}
-}
 
 func TestEqual(t *testing.T) {
 
 	//Test equal blocks
-	blkA := New(0, "data", nil)
+	blkA := New(0, []byte("data"), nil)
 	blkB := blkA
-
+	log.Println("Testing Block Equality")
 	if !Equal(blkA, blkB) {
 		t.Error("Block equivilents do not match")
 	}
 
 	//Test unequal blocks
-	blkC := New(1, "str", nil)
+	log.Println("Testing Block Inequality")
+	blkC := New(1, []byte("str"), nil)
 
 	if Equal(blkA, blkC) {
 		t.Error("Unequivilent blocks returning as matching")
 	}
+
 }
