1f1a68faf1010f45a804b66b994e0f0846723f0f
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-11-17 17:54:51 -0500

    add setroot command

--- app/app.go
+++ app/app.go
@@ -1,31 +1,75 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
+	"fmt"
+
+	"path/filepath"
+
+	"github.com/LaughingCabbage/goLinks/types/walker"
 	"github.com/urfave/cli"
 )
 
+const (
+	//walkerWorkers int    = 1
+	defaultRoot string = ""
+	configName  string = "config"
+)
+
+var w = walker.New(defaultRoot)
+
 func main() {
 	app := cli.NewApp()
-	app.Name = "golink s"
+	app.Name = "golink"
 	app.Usage = "filesystem blockchain"
 
 	app.Commands = []cli.Command{
 		{
+			Name:    "root",
+			Aliases: []string{"r"},
+			Usage:   "set walker archive root",
+			Action:  appSetRoot,
+		},
+		{
 			Name:    "walk",
 			Aliases: []string{"w"},
 			Usage:   "walk and print archive",
-			Action: func(c *cli.Context) error {
-				fmt.Println("Walking")
-				return nil
-			},
+			Action:  appWalk,
 		},
 	}
 
+	//legooo
 	if err := app.Run(os.Args); err != nil {
 		panic(err)
 	}
 
+}
+
+func appSetRoot(c *cli.Context) error {
+	fmt.Printf("Adding walker root: %v\n", c.Args().First())
+	//Validate archive root
+	if _, err := os.Stat(c.Args().First()); err != nil {
+		if os.IsNotExist(err) {
+			return cli.NewExitError("Add: archive root does not exit", 0)
+		}
+		panic(err)
+	}
+
+	//TODO is this check needed or can c.args be used?
+	//Extract valid path for filesystem.
+	path, err := filepath.Abs(c.Args().First())
+	if err != nil {
+		return cli.NewExitError(err, 0)
+	}
+	w = walker.New(path)
+	return nil
+}
+
+func appWalk(c *cli.Context) error {
+	if err := w.Walk(); err != nil {
+		return cli.NewExitError(err.Error(), 0)
+	}
+	w.PrintArchive()
+	return nil
 }
