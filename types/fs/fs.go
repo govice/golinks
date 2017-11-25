05fd8bc0665dbcf29aaff5ca5ae0c8911f670d78
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-11-25 17:44:44 -0500

    wrap errors and fix bug with close

--- types/fs/fs.go
+++ types/fs/fs.go
@@ -1,38 +1,42 @@
 package fs
 
 import (
-	"os"
-
 	"crypto/sha512"
+	"os"
 
 	"github.com/pkg/errors"
 )
 
+//HashFile returns a sha512 hash of the file at the provided path
 func HashFile(path string) ([]byte, error) {
 	//If path is null return
 	if path == "" {
-		return nil, errors.New("BlockMap: failed to hash null path")
+		return nil, errors.New("fs: failed to hash null path")
 	}
 	//Open open and verify file in path
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, errors.Wrap(err, "BlockMap: failed to read "+path)
+		return nil, errors.Wrap(err, "fs: failed to read "+path)
 	}
 
 	//Get the file size for reading
 	fileStat, err := f.Stat()
 	if err != nil {
-		return nil, errors.Wrap(err, "BlockMap: failed read size of "+path)
+		return nil, errors.Wrap(err, "fs: failed read size of "+path)
 	}
 	//Construct a buffer based on the file size
 	buffer := make([]byte, fileStat.Size())
 	//Read the file and verify bytes read equal the stat size
 	bytesRead, err := f.Read(buffer)
 	if err != nil {
-		return nil, errors.Wrap(err, "BlockMap: failed to to buffer in file "+path)
+		return nil, errors.Wrap(err, "fs: failed to to buffer in file "+path)
 	}
 	if bytesRead != int(fileStat.Size()) {
-		return nil, errors.New("Blockmap: bytes read not equal to stat size of file" + path)
+		return nil, errors.New("fs: bytes read not equal to stat size of file" + path)
+	}
+
+	if err := f.Close(); err != nil {
+		return nil, errors.Wrap(err, "fs: failed to close file "+path)
 	}
 
 	fileHash := sha512.New()
