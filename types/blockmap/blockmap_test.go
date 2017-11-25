6e65b670178f77c2fd60fd8436c3c9705230e002
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-11-25 17:44:01 -0500

    add test for new blockmap

--- types/blockmap/blockmap_test.go
+++ types/blockmap/blockmap_test.go
@@ -1,1 +1,14 @@
 package blockmap
+
+import (
+	"errors"
+	"os"
+	"testing"
+)
+
+func TestBlockMap_New(t *testing.T) {
+	a := New(os.Getenv("TEST_ROOT"))
+	if a == nil {
+		t.Error(errors.New("Blockmap: failed to make new blockmap"))
+	}
+}
