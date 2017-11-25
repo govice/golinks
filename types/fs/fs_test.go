971cf99b6ea9117df268ec2f4bab52af318ade9b
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-11-25 17:45:32 -0500

    remove prints

--- types/fs/fs_test.go
+++ types/fs/fs_test.go
@@ -1,13 +1,11 @@
 package fs
 
 import (
-	"fmt"
 	"io/ioutil"
 	"math/rand"
-	"testing"
-
 	"os"
 	"path/filepath"
+	"testing"
 	"time"
 
 	"github.com/pkg/errors"
@@ -22,15 +20,14 @@ func TestHashFile(t *testing.T) {
 		t.Error(err)
 	}
 	//remove files after testing
-	/*
-		defer func() {
-			if err := os.RemoveAll(TestPath); err != nil {
-				t.Error(err)
-			}
-		}()
-	*/
-	//Generate Archive
 
+	defer func() {
+		if err := os.RemoveAll(TestPath); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	//Generate Archive
 	tmpdir, err := ioutil.TempDir(TestPath, "testDir")
 	if err != nil {
 		t.Error(err)
@@ -44,20 +41,22 @@ func TestHashFile(t *testing.T) {
 		t.Error(err)
 	}
 
-	if _, err := tmpfile.Write(buff); err != nil {
-		t.Error(err)
-	}
-	if err := tmpfile.Close(); err != nil {
+	if _, err = tmpfile.Write(buff); err != nil {
 		t.Error(err)
 	}
 
+	if err = tmpfile.Close(); err != nil {
+		t.Error(err)
+	}
 	//end setup
+
+	//Run hash
 	hash, err := HashFile(tmpfile.Name())
-	fmt.Println(hash)
 	if err != nil {
 		t.Error(err)
 	}
 	if hash == nil {
 		t.Error(errors.New("fs: HashFile returned nil hash"))
 	}
+
 }
