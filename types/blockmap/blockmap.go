18eaf7a4e16428b9e851df600982f1cbab7bf072
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-11-29 16:07:32 -0500

    export blockmap children. add load and save

--- types/blockmap/blockmap.go
+++ types/blockmap/blockmap.go
@@ -1,6 +1,7 @@
 package blockmap
 
 import (
+	"log"
 	"path/filepath"
 
 	"bytes"
@@ -10,28 +11,35 @@ import (
 
 	"fmt"
 
+	"os"
+
 	"github.com/LaughingCabbage/goLinks/types/fs"
 	"github.com/LaughingCabbage/goLinks/types/walker"
 	"github.com/pkg/errors"
 )
 
+//OutputName stores the default file name archive metadata
+const OutputName string = ".link"
+
 //BlockMap is a ad-hoc Merkle tree-map
 type BlockMap struct {
-	archive  map[string][]byte
-	rootHash []byte
-	root     string
+	Archive  map[string][]byte
+	RootHash []byte
+	Root     string
 }
 
 //New returns a new BlockMap initialized at the provided root
 func New(root string) *BlockMap {
+	//Initialize map and assign blockmap root
 	rootMap := make(map[string][]byte)
-	return &BlockMap{archive: rootMap, rootHash: nil, root: root}
+	return &BlockMap{Archive: rootMap, RootHash: nil, Root: root}
 }
 
 //Generate creates an archive of the provided archives root filesystem
 func (b *BlockMap) Generate() error {
-
-	w := walker.New(b.root)
+	//Create a filesystem walker
+	w := walker.New(b.Root)
+	//Walk the root directory
 	if err := w.Walk(); err != nil {
 		return errors.Wrap(err, "BlockMap: failed to walk "+w.Root())
 	}
@@ -51,7 +59,7 @@ func (b *BlockMap) Generate() error {
 		}
 
 		//Add the hash to the archive using the relative path as it's key
-		b.archive[relPath] = fileHash
+		b.Archive[relPath] = fileHash
 	}
 
 	//If we're here, the entries are successful so we'll hash the blockmap.
@@ -64,45 +72,80 @@ func (b *BlockMap) Generate() error {
 }
 
 func (b *BlockMap) hashBlockMap() error {
-	if b.archive == nil {
+	//Make sure an archive exists
+	if b.Archive == nil {
 		return errors.New("hashBlockMap: Attempted to hash null archive")
 	}
+
+	//Begin hashing blockmap gob
 	hash := sha512.New()
 	buffer := new(bytes.Buffer)
 	encoder := gob.NewEncoder(buffer)
-
-	err := encoder.Encode(b.archive)
+	err := encoder.Encode(b.Archive)
 	if err != nil {
 		return errors.Wrap(err, "hashBlockMap: failed to encode archive map")
 	}
-
 	if _, err := hash.Write(buffer.Bytes()); err != nil {
 		return errors.Wrap(err, "hashBlockMap: failed to write to write hash buffer")
 	}
 
-	b.rootHash = hash.Sum(nil)
+	b.RootHash = hash.Sum(nil)
 	return nil
 
 }
 
 //PrintBlockMap prints an existing block map and returns an error if not configured
 func (b BlockMap) PrintBlockMap() {
-	if b.rootHash == nil {
-		fmt.Println("BlockMap is unhashed or unset")
+	if b.RootHash == nil {
+		log.Println("BlockMap is unhashed or unset")
 	}
-	fmt.Println("Root: " + b.root)
-	fmt.Printf("Hash: %v\n", b.rootHash)
-	for key, value := range b.archive {
+	fmt.Println("Root: " + b.Root)
+	fmt.Printf("Hash: %v\n", b.RootHash)
+	for key, value := range b.Archive {
 		fmt.Printf("%v: %v\n", key, value)
 	}
 }
 
+//Save will store a byte file of the blockmap in the default OutputFile
+func (b BlockMap) Save(path string) error {
+	if b.RootHash == nil {
+		return errors.New("BlockMap: can't save nil hashed map")
+	}
+	file, err := os.OpenFile(path+string(os.PathSeparator)+OutputName, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return errors.Wrap(err, "BlockMap: failed to save link file")
+	}
+	encoder := gob.NewEncoder(file)
+	if err = encoder.Encode(b); err != nil {
+		return errors.Wrap(err, "BlockMap: failed to encode link file")
+	}
+	if err := file.Close(); err != nil {
+		return errors.Wrap(err, "BlockMap: failed to close link file")
+	}
+	return nil
+}
+
+//Load reads the blockmap from the default OutputFile
+func (b *BlockMap) Load(path string) error {
+	file, err := os.Open(path + string(os.PathSeparator) + OutputName)
+	if err != nil {
+		return errors.Wrap(err, "BlockMap: failed to open link file")
+	}
+	decoder := gob.NewDecoder(file)
+	if err = decoder.Decode(b); err != nil {
+		return errors.Wrap(err, "BlockMap: failed to decode blockmap")
+	}
+	if err = file.Close(); err != nil {
+		return errors.Wrap(err, "BlockMap: failed to close link file")
+	}
+
+	return nil
+}
+
 //Equal returns an evaluation of the equality of two blockmaps
 func Equal(a, b *BlockMap) bool {
-	if a.root != b.root && !bytes.Equal(a.rootHash, b.rootHash) {
+	if a.Root != b.Root && !bytes.Equal(a.RootHash, b.RootHash) {
 		return false
 	}
 	return true
 }
-
-//TODO verify existing blockmap
