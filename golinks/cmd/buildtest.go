95d19d17cce72feeed5dcb57836a389bcab3c9af
Kevin Gentile <gentile_kevin94@hotmail.com>
2018-11-26 21:16:03 -0500

    implement buildtest [Resolves #12]
    begin config file
    
    add and read in config file
    
    begin adding buildtest
    
    move to tower
    
    add config print command
    
    handle buildtest flags. log.
    
    generate test folder. move config and test to hidden golinks folder
    
    use os path seperators for default config
    
    build out test directories

---
+++ golinks/cmd/buildtest.go
@@ -0,0 +1,126 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var buildTestCmd = &cobra.Command{
+	Use:   "buildtest",
+	Short: "Build out an archive to test on",
+	Long:  "Build out an archive to test on based according to the preferred size",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := buildTestDir(cmd.Flag("size").Value.String()); err != nil {
+			log.Println(err)
+			cmd.Help()
+		}
+	},
+}
+
+const (
+	testDirs    int = 10
+	testDirSize int = 10
+	smallFile   int = 1                 //1 B
+	mediumFile  int = smallFile * 1024  //1 KB
+	largeFile   int = mediumFile * 1024 //1 MB
+)
+
+var testSize string
+
+// TODO config property
+var testPath = string(os.PathSeparator) + "testing"
+
+type test struct {
+	ArchiveSize   int
+	DirectorySize int
+	FileSize      int
+}
+
+//TODO cleanup of testing environment
+func buildTestDir(size string) error {
+	var testConfig test
+	switch size {
+
+	case "small":
+		testConfig = test{testDirs, testDirSize, smallFile}
+		log.Print("generating small test")
+
+	case "medium":
+		testConfig = test{testDirs, testDirSize, mediumFile}
+		log.Println("generating medium test")
+
+	case "large":
+		testConfig = test{testDirs, testDirSize, largeFile}
+		log.Println("generating large test")
+	default:
+		return errors.New("Build test size is not recognized")
+	}
+
+	testPath := viper.Get("testpath").(string)
+	os.Mkdir(testPath, 0755)
+	if good, err := verifyPath(testPath); !good || err != nil {
+		if err != nil {
+			return err
+		}
+		return errors.New("Failed to verify config test directory at " + testPath)
+	}
+
+	if err := generateTestDir(testPath, testConfig); err != nil {
+		return err
+	}
+	fmt.Println(testConfig)
+	return nil
+}
+
+// TODO maybe not random data?
+func generateTestDir(testRoot string, t test) error {
+	log.Printf("Test Archive Size: %v", t.ArchiveSize)
+	log.Println("Test Root: " + testRoot)
+	for i := 0; i < t.ArchiveSize; i++ {
+		iStr := strconv.Itoa(i)
+		log.Println("Creating Archive " + iStr)
+		tmpdir, err := ioutil.TempDir(testRoot, "test"+iStr)
+		if err != nil {
+			return err
+		}
+		//Write random data to files
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		for j := 0; j < t.DirectorySize; j++ {
+			jStr := strconv.Itoa(j)
+			buff := make([]byte, t.FileSize)
+			r.Read(buff)
+			tmpfile, err := ioutil.TempFile(tmpdir, "file"+jStr)
+			if err != nil {
+				panic(err)
+			}
+
+			if _, err := tmpfile.Write(buff); err != nil {
+				panic(err)
+			}
+			if err := tmpfile.Close(); err != nil {
+				panic(err)
+			}
+		}
+	}
+	return nil
+}
+
+func verifyPath(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
