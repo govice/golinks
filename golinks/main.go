c56d95cd74754e64c40e698a217b3c3086e4efd2
Kevin Gentile <gentile_kevin94@hotmail.com>
2018-12-16 20:48:18 -0500

    [Closes #39] cmd: move CLI to root

--- golinks/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/laughingcabbage/golinks/golinks/cmd"
-
-func main() {
-	cmd.Execute()
-}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/laughingcabbage/golinks/cmd"
+
+func main() {
+	cmd.Execute()
+}
